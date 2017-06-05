9ba33aec2c1ae7f18b8509cb4733cfb01402bc1d
Wanwannodao <lavilabyrinth@yahoo.co.jp>
2017-06-05 21:24:19 +0900

    ckpt

--- RNN/PtrNet/main.py
+++ RNN/PtrNet/main.py
@@ -14,7 +14,7 @@ class Input():
             enc_data, dec_data, batch_size, name=name)
 
 class Config():
-    batch_size  = 20
+    batch_size  = 200
     num_steps   = 50 + 1 # number of indices + stop symbol
     hidden_size = 256
 
@@ -29,15 +29,22 @@ def main(_):
                     input_=input_)
 
     with tf.Session() as sess:
+
+        saver = tf.train.Saver()
+        
         sess.run(tf.global_variables_initializer())
         
         coord   = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-        for _ in range(1000):
-            sess.run(ptrnet.train_op)
+        for epoch in range(100):
+            for _ in range(2500):
+                sess.run(ptrnet.train_op)
+                
             loss = sess.run(ptrnet.loss)
-            print(loss)
+            print("epoch %d " % loss)
+
+            saver.save(sess, "./checkpoint/model.ckpt", global_step=global_step)           
         
         coord.request_stop()
         coord.join(threads)
